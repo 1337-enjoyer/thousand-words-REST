92f553533a11ccb6ba7d40c19adc5ea807f32a24
Anton Silaev <s1laev-aa@yandex.ru>
2026-05-04 19:26:12 +0300

    remove large VSCode zip file and update game endpoint

--- thousand_words/api/views.py
+++ thousand_words/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response 
 
 from words.models import Words
@@ -7,11 +8,16 @@ from .game_logic import get_game_question
 from .serializers import WordsSerializer
 
 
+class WordsPagination(PageNumberPagination):
+    page_size = 10
+
 class WordViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Words.objects.all()
     serializer_class = WordsSerializer
-
+    pagination_class = WordsPagination
+    
     @action(detail=False, methods=['get'], url_path='game')
     def game(self, request):
         result = get_game_question(self.get_queryset())
         return Response(result)
+
