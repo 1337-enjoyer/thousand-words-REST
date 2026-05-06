aee669ab11bb971d86dcc7535985811682127ace
Anton Silaev <s1laev-aa@yandex.ru>
2026-05-06 15:43:50 +0300

    Add redoc for API

--- thousand_words/api/game_logic.py
+++ thousand_words/api/game_logic.py
@@ -1,22 +1,20 @@
 import random
-from rest_framework.response import Response
-from rest_framework import status
 
 
 def get_game_question(words_queryset):
     all_words = list(words_queryset)
     target_word = random.choice(all_words)
-    
+
     other_words = [w for w in all_words if w.id != target_word.id]
     random_others = random.sample(other_words, 2)
-    
+
     translations = [
         random_others[0].translation,
         random_others[1].translation,
         target_word.translation
     ]
     random.shuffle(translations)
-    
+
     return {
         "word": target_word.word,
         "translations": translations,
