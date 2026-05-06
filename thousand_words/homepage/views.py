c4081c5d263039fb72869bc491ba479c8426ed22
Anton Silaev <s1laev-aa@yandex.ru>
2026-05-06 18:48:55 +0300

    Registration, CBV homepage

--- thousand_words/homepage/views.py
+++ thousand_words/homepage/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, TemplateView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-def index(request):
+
+class IndexView(TemplateView):
     template_name = 'homepage/index.html'
-    return render(request, template_name) 
+
+
+class UserCreateView(CreateView):
+    template_name = 'registration/registration_form.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('pages:homepage')
--- thousand_words/thousand_words/urls.py
+++ thousand_words/thousand_words/urls.py
@@ -4,6 +4,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from homepage.views import UserCreateView
+
 schema_view = get_schema_view(
     openapi.Info(
         title=f"Thousand word API",
@@ -19,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path('auth/registration', UserCreateView.as_view(), name='registration'),
     path('api/', include('api.urls')),
     path(
         'redoc/', 
